62daa1e6696932f87aa1b54b6c71816bb95bf59e
mini2809 <minijain928@gmail.com>
2022-05-22 23:08:00 +0530

    Backend for contacts

---
+++ server.js
@@ -0,0 +1,64 @@
+const dataset = require("./dataset.json");
+const cors = require("cors");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const port = 6700;
+
+let returnArray = [];
+app.use(cors());
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/contacts/", (req, res) => {
+	returnArray = [];
+	let favorite = req.query.favorite;
+	if (favorite == "true") {
+		for (let i = 0; i < dataset.length; i++) {
+			for (const [key, value] of Object.entries(dataset[i])) {
+				if (key == "favorite" && value == true)
+					returnArray.push(dataset[i]);
+			}
+		}
+		res.send(returnArray);
+		return;
+	}
+
+	let perpage = req.query.perpage != "" ? parseInt(req.query.perpage) : 10;
+	let page = req.query.page != "" ? parseInt(req.query.page) : 1;
+
+	console.log(req.query);
+	if (req.query.keyword != undefined && req.query.keyword != "") {
+		for (let i = 0; i < dataset.length; i++) {
+			for (const [key, value] of Object.entries(dataset[i])) {
+				if (
+					key == "name" &&
+					req.query.keyword.toLowerCase() == value.toLowerCase()
+				)
+					returnArray.push(dataset[i]);
+			}
+		}
+		res.send(returnArray);
+		return;
+	}
+
+	let location = req.query.location !== undefined ? req.query.location : "";
+	let tags = req.query.tags !== undefined ? req.query.tags : "";
+	let status = req.query.status !== undefined ? req.query.status : "";
+
+	for (let i = 0; i < dataset.length; i++) {
+		for (const [key, value] of Object.entries(dataset[i])) {
+			if (
+				(key === "location" &&
+					(location == "" || value === location)) ||
+				(key === "status" && (status == "" || value === status))
+			) {
+				returnArray.push(dataset[i]);
+				break;
+			}
+		}
+	}
+	res.send(returnArray);
+	return returnArray;
+});
